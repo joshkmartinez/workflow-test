e2cb4713eaea84ba860d8857aa311e54dcc39be8
Joshua <joshkmartinez@gmail.com>
2023-07-21 11:34:24 -0700

    fix release endpoint

--- .github/prReleaseComment.js
+++ .github/prReleaseComment.js
@@ -1,23 +1,26 @@
-// commentOnPRs.js
 async function commentOnPRs(github, context) {
-    const newTag = context.ref.replace('refs/tags/', '');
-    const { data: release } = await github.repos.getReleaseByTag({
+  const newTag = context.ref.replace('refs/tags/', '');
+  const releases = await github.repos.listReleases({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+  });
+  const release = releases.data.find((rel) => rel.tag_name === newTag);
+
+  if (!release) {
+    console.log(`Release not found for tag ${newTag}`);
+    return;
+  }
+
+  const prNumbers = release.body.match(/\(#(\d+)\)/g).map((match) => match.replace(/\D/g, ''));
+
+  for (const prNumber of prNumbers) {
+    await github.issues.createComment({
       owner: context.repo.owner,
       repo: context.repo.repo,
-      tag: newTag,
+      issue_number: prNumber,
+      body: `This PR has been included in release: ${newTag}, see the [release notes](https://github.com/${context.repo.owner}/${context.repo.repo}/releases/tag/${newTag}).`,
     });
-  
-    const prNumbers = release.body.match(/\(#(\d+)\)/g).map((match) => match.replace(/\D/g, ''));
-  
-    for (const prNumber of prNumbers) {
-      await github.issues.createComment({
-        owner: context.repo.owner,
-        repo: context.repo.repo,
-        issue_number: prNumber,
-        body: `This PR has been included in release: ${newTag}, see the [release notes](https://github.com/${context.repo.owner}/${context.repo.repo}/releases/tag/${newTag}).`,
-      });
-    }
   }
-  
-  module.exports = commentOnPRs;
-  
+}
+
+module.exports = commentOnPRs;
